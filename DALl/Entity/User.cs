b92d543f7d1c487ac018cd7da48fcf16e9718595
Islambek123 <topkokos1337@gmail.com>
2018-03-21 15:40:55 +0200

    sdadsa

---
+++ DALl/Entity/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALl.Entity
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(maximumLength: 200)]
+        public string NickName { get; set; }
+        [Required, StringLength(maximumLength: 200)]
+        public string Email { get; set; }
+        [Required, StringLength(maximumLength: 200)]
+        public string Password { get; set; }
+        [Required]
+        public int Phone { get; set; }
+        [Required]
+        public float Mony { get; set; }
+    }
+}
---
+++ baraholca/Website/js/Login.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    //document.getElementById('cont').onclick = function () { CheckEmpty() };
+
+    //function CheckEmpty() {
+    //    if (document.getElementById('un').textContent == "" || document.getElementById('pw').value == "") {
+    //        alert("Дані введено не коректно");
+    //    } else if (document.getElementByName('un').value == "admin" && document.getElementById('pw').value == "123456") {
+    //        alert("Привіт Адмінам");
+    //    }
+    //}
+}
